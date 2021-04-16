246ec02918204c45210aea20a2c97436e0070e98
hilahersz <hila@trustdome.com>
2021-04-16 16:04:09 +0300

    feat: first clean pdf tests [skip ci]

--- tests/test_clean_pdf.py
+++ tests/test_clean_pdf.py
@@ -2,12 +2,20 @@
 A test module for PDF cleanup
 """
 
-import os
-from contextlib import contextmanager
-from tempfile import TemporaryDirectory
-from unittest import TestCase, mock
+from unittest import TestCase
+
+from final_project.data_collection.tasks.data_cleanup.clean_pdf import (
+    get_text_without_non_ascii_chars,
+)
 
 
 class TestCleanPdf(TestCase):
-    def test_pdf_cleanup_regression(self):
-        pass
+    def test_text_with_non_ascii_values_should_get_clean_text(self):
+        """
+        Tests the correct removal of non-ascii symbols
+        """
+        dirty_text = "Hello\000 World !"
+        expected_text = "Hello World !"
+        actual_text = get_text_without_non_ascii_chars(dirty_text)
+
+        self.assertEqual(expected_text, actual_text)
