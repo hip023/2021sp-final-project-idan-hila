f2775612149d25ded6398a52109c76d3318d24e4
hilahersz <hila@trustdome.com>
2021-04-24 17:17:43 +0300

    fix: styled django template

---
+++ SearchEngine/SearchEngine/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from final_project.es_wrapper.es_wrapper import es_results_wrapper
+
+
+def homepage(request):
+    return render(request, 'home.html')
+
+
+def results(request):
+    if request.method == "POST":
+        query = request.POST.get('search')
+        if query:
+            results = es_results_wrapper(query)
+            return render(request, 'results.html', {'pdf': results})
